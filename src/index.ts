b5a9b6a43cb6d6817921fb0dd6d1a21cca8de0b7
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-08 11:25:53 +0900

    feat: comcigan raw data 가져오기

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
 import ComciganError from "./error.js";
 import axios from "axios";
 import iconv from "iconv-lite";
-import { ComciganSchool, ComciganSearchRaw } from "./types/index.js";
+import { ComciganRawData, ComciganSchool, ComciganSearchRaw } from "./types/index.js";
+axios.defaults.validateStatus = () => true;
 
 export default class Comcigan {
-    constructor() {
+    constructor(
+        public code: number
+    ) { }
 
-    }
+    private _data?: ComciganRawData;
 
+    /**
+     * 학교를 검색합니다.
+     * @param query 검색어
+     * @returns 검색 결과
+     */
     public static async search(query: string) {
         const euckr = Array.from(iconv.encode(query, "euc-kr"))
             .map(byte => '%' + byte.toString(16).toUpperCase().padStart(2, '0'))
             .join('');
 
-        const r = await axios.get<string>("http://comci.net:4082/36179?17384l" + euckr, {
-            "validateStatus": status => {
-                if (status !== 200) throw new ComciganError(status, `${query}의 검색에 실패했습니다.`);
-                return true;
-            }
-        });
-
+        const r = await axios.get<string>("http://comci.net:4082/36179?17384l" + euckr);
         const obj: ComciganSearchRaw = JSON.parse(r.data.substring(0, r.data.lastIndexOf("}") + 1));
         return obj.학교검색
             .filter(v => v[3] !== 0)
@@ -28,5 +30,29 @@ export default class Comcigan {
                 "region": v[1],
                 "name": v[2]
             } satisfies ComciganSchool));
+    }
+
+    private async _getdata() {
+        if (this._data) return;
+
+        const route = btoa(`73629_${this.code}_0_1`);
+
+        const r = await axios.get<string>(`http://comci.net:4082/36179?${route}`);
+
+        if (r.data.startsWith("{}")) throw new ComciganError(`학교 정보를 가져오는데 실패했어요. 학교 코드가 올바른지 확인해주세요.`);
+
+        const obj: ComciganRawData = JSON.parse(r.data.substring(0, r.data.lastIndexOf("}") + 1));
+
+        this._data = obj;
+    }
+
+    public async schoolInfo() {
+
+    }
+
+    public async timetable(grade: number, classNum: number) {
+        await this._getdata();
+
+
     }
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,4 +6,71 @@ export type ComciganSchool = {
 
 export type ComciganSearchRaw = {
     "학교검색": [number, string, string, number][];
+};
+
+export type ComciganTimetableItem = {
+    "subject": string,
+    "teacher": string,
+    "classRoom"?: string,
+    "original"?: Omit<ComciganTimetableItem, "original">,
+}
+
+export type ComciganTimetable = [
+    ComciganTimetableItem[], // 월요일
+    ComciganTimetableItem[], // 화요일
+    ComciganTimetableItem[], // 수요일
+    ComciganTimetableItem[], // 목요일
+    ComciganTimetableItem[]  // 금요일
+]
+
+type NestedValue =
+    | string
+    | number
+    | NestedValue[]
+    | { [key: string]: NestedValue };
+
+export interface ComciganRawData {
+    "교사수": number;
+    "자료446": string[];
+    "학급수": number[];
+    "요일별시수": number[][];
+
+    "자료492": (string | number)[];
+    "자료481": NestedValue[];
+    "전일제": number[];
+
+    "버젼": string;
+    "동시수업수": number;
+
+    "담임": number[][];
+    "가상학급수": number[];
+    "전체학년": number[];
+
+    "특별실수": number;
+    "열람제한일": string;
+    "자료244": string;
+
+    "학기시작일자": string;
+    "학교명": string;
+    "지역명": string;
+    "학년도": number;
+
+    "분리": number;
+    "강의실": number;
+    "소규모학급": number;
+    "변경알림": number;
+
+    "시작일": string;
+
+    "일과시간": string[];
+
+    "일자자료": [number, string][];
+
+    "오늘r": number;
+
+    "자료147": NestedValue[];
+    "자료542": NestedValue[];
+    "자료245": NestedValue[];
+
+    "동시그룹": (number | number[])[];
 }
