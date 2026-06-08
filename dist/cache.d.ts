a3a0e28c53f5f5bd7aa69c5dfeeec2e372134fd7
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-08 11:00:21 +0900

    fix: dist ignore

--- dist/cache.d.ts
+++
@@ -1,8 +0,0 @@
-export default class Cache {
-    private _cacheStorage;
-    register(id: string, data: any, duration: number): void;
-    delete(id: string): void;
-    clear(): void;
-    has(id: string): boolean;
-    get(id: string): any;
-}
