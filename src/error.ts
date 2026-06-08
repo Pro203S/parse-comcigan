b97763136453673eb93ad3f69dc423d2d5618a04
prosirea2501@gmail.com <prosirea2501@gmail.com>
2026-06-08 11:25:42 +0900

    chore: error.ts 수정

--- src/error.ts
+++ src/error.ts
@@ -1,8 +1,7 @@
 export default class ComciganError extends Error {
     constructor(
-        public status: number,
         public message: string
-    ) { super(`${status}: ${message}`); }
+    ) { super(message); }
 
     
 }
